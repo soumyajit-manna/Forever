be08addc5c207acd3eef4eeb9420bebb2514cc0e
Soumyajit Manna <122715874+soumyajit-manna@users.noreply.github.com>
2025-07-22 11:24:56 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,10 +8,9 @@ const Footer = () => {
         <div>
           <img src={assets.logo} className="mb-5 w-32" alt="" />
           <p className="w-full md:w-2/3 text-gray-600">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            FOREVER — Your trusted fashion destination.
+            Bringing timeless style since the 1500s, inspired by the legacy of classic design and craftsmanship.
+            From vintage elegance to modern trends, we've got you covered.
           </p>
         </div>
 
